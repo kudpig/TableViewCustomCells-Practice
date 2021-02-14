c79772bc319be35460363533bd36b52a102a4f18
kudpig <shin.kudo.aa@gmail.com>
2021-02-14 14:12:31 +0900

    Set_Normal_TableView

--- TableViewCustomCells-Practice/ViewController.swift
+++ TableViewCustomCells-Practice/ViewController.swift
@@ -7,13 +7,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet var tableView: UITableView!
+    let myData = ["first", "second", "third", "four", "five"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
     }
 
+    // TableView Funcs
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return myData.count
+    }
 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = myData[indexPath.row]
+        
+        return cell
+    }
+    
 }
 
