155f0659a7e011adf9e884bc8671712948552634
kudpig <shin.kudo.aa@gmail.com>
2021-02-14 14:13:40 +0900

    [create]DemoTableViewCell_File&xib

---
+++ TableViewCustomCells-Practice/DemoTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  DemoTableViewCell.swift
+//  TableViewCustomCells-Practice
+//
+//  Created by shinichiro kudo on 2021/02/14.
+//
+
+import UIKit
+
+class DemoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
